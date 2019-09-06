759e8d701027d2a1f61358e68da9f507f37b67ff
李跃东 <app@codeboy.me>
2019-09-06 21:14:15 +0800

    Update Room.java
    修正序列化问题

--- src/main/java/me/codeboy/clipboard/data/Room.java
+++ src/main/java/me/codeboy/clipboard/data/Room.java
@@ -1,5 +1,6 @@
 package me.codeboy.clipboard.data;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -8,11 +9,13 @@ import java.util.Objects;
  * 房间数据
  * Created by yuedong.li on 2019-07-12
  */
-public class Room {
+public class Room implements Serializable {
     public String name;
-    public String password;
-    public List<String> contents = new ArrayList<>();
-    public final static int MAX_SIZE = 100;
+    private String password;
+    private List<String> contents = new ArrayList<>();
+    private long latestAccessTime = System.currentTimeMillis();
+    private long latestUpdateTime = System.currentTimeMillis();
+    private final static int MAX_SIZE = 100;
 
     public Room() {
         contents.add("欢迎使用，您可以粘贴文本或者图片，进而共享给其他的设备");
@@ -34,6 +37,33 @@ public class Room {
         this.password = password;
     }
 
+    public void setContents(List<String> contents) {
+        this.contents = contents;
+    }
+
+    public long getLatestAccessTime() {
+        return latestAccessTime;
+    }
+
+    public void setLatestAccessTime(long latestAccessTime) {
+        this.latestAccessTime = latestAccessTime;
+    }
+
+    public void setLatestUpdateTime(long latestUpdateTime) {
+        this.latestUpdateTime = latestUpdateTime;
+    }
+
+    /**
+     * 删除内容
+     *
+     * @param content 内容
+     */
+    public synchronized void removeContent(String content) {
+        if (content != null) {
+            contents.remove(content);
+        }
+    }
+
     /**
      * 添加内容
      *
@@ -47,6 +77,13 @@ public class Room {
             contents.remove(contents.size() - 1);
         }
         contents.add(0, content);
+        latestUpdateTime = System.currentTimeMillis();
+        latestAccessTime = latestUpdateTime;
+    }
+
+    public List<String> getContents() {
+        latestAccessTime = System.currentTimeMillis();
+        return new ArrayList<>(contents);
     }
 
     /**
@@ -59,6 +96,19 @@ public class Room {
         room.name = name;
         room.password = password;
         return room;
+    }
+
+    /**
+     * 是否需要删除，一天内没有访问数据删除
+     *
+     * @return 结果
+     */
+    public boolean needDelete() {
+        return System.currentTimeMillis() - latestAccessTime > 24 * 3600 * 1000;
+    }
+
+    public long getLatestUpdateTime() {
+        return latestUpdateTime;
     }
 
     @Override
